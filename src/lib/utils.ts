0ca576ced1044d65693eb5f575ea6ea1360ed27a
p a <marketingopen10@gmail.com>
2025-06-17 18:55:12 +0000

    the app doesnt start . Fix it!

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "./tailwindMerge"; // Updated import path
+import { twMerge } from "tailwind-merge"; // Direct import
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
